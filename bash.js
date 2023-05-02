0ab7abfc5e611884aafa5dfccaf10611a8709d43
Gregory <gregster0@gmail.com>
2023-05-02 01:32:51 +0000

    stuff

---
+++ bash.js
@@ -0,0 +1,15 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+    process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+    process.stdin.on('data', (data) => {
+      const cmd = data.toString().trim(); //remove the newline
+
+    if(cmd === 'pwd')
+      pwd();
+    
+    if(cmd === 'ls')
+      ls();
+    });
